96379301b83ed617ce7e0a8cadc13af805565c1f
Harsh Patel <hhpatel1509@gmail.com>
2024-02-19 09:15:21 +0530

    distinct multification problem done

--- Day5/temp.py
+++ Day5/temp.py
@@ -15,3 +15,21 @@ def find_con(a):
     
 print(find_con([3,2,6,4,5]))
 print(find_con([1,3,2,5]))
+
+
+def dismulti(a):
+    a=str(a)
+    n=len(a)
+    hs=set()
+    for i in range(n):
+        prod=1
+        for j in range(i,n):
+            prod=prod*int(a[j])
+            if(prod in hs):
+                return 0
+            else:
+                hs.add(prod)
+    return 1
+
+print(dismulti(23))
+print(dismulti(236))
